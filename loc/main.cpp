b663cca51dc882173a0e86255f6101f013ba2a08
c0ldzy <90956654+c0ldzy@users.noreply.github.com>
2021-09-22 10:43:46 +0300

    Update main.cpp

--- loc/main.cpp
+++ loc/main.cpp
@@ -1,30 +1,64 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 using namespace std;
 
-void results_by_name(){
-    freopen("as_is", "r", stdin);
-        getline(cin, s);
-        while (getline(cin, s)){
+string s;
+
+void results_by_name()
+{
+    ifstream x("outputlol.txt", ios_base::in);
+
+    while (getline(x, s)){
+        cout << s + '\n';
+    }
+}
+
+void quiz_by_name()
+{
+    ifstream y("inputlol.txt", ios_base::in);
+    ofstream x("outputlol.txt", ios_base::app);
+
+
+    while (getline(y, s)){
             cout << s + '\n';
+            getline(cin, s);
+            x << s << endl;
         }
 }
-void quiz_by_name(){
-    freopen("inputfile", "r", stdout);
-        getline(cin, s);
-        while (getline(cin, s)){
-            cout << s + '\n';
-        }
+
+void change_questions(){
+    cout << "current questions:" << endl;
+
+    ifstream y("inputlol.txt", ios_base::in);
+
+    while (getline(y, s))  cout << s << endl;
+
+    cout << endl;
+
+    cout << "Now enter what u want questions to look like (enter LOSEN to end)" << endl;
+
+    ofstream x("inputlol.txt", ios_base::trunc);
+
+    while(cin >> s) {if(s == "LOSEN") break; x << s << endl;}
 }
-int main(){
-    string s;
+
+int main()
+{
+    cout << "Wanna change questions? (yes or no)" << endl;
+    cin >> s;
+
+    if(s == "yes") change_questions();
+
     cout << "Choose poll_results or quiz\n";
     cin >> s;
+
     //не понял как сортировать данные по имени пользователя
+
     if (s == "poll_results"){
         results_by_name();
     }else if (s == "quiz"){
         quiz_by_name();
     }
-    cout << "Statistics\n People passed the poll: 1\n Unique books: 1\n"
+    cout << "Statistics\n People passed the poll: 1\n Unique books: 1\n";
 }
